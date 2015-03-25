bcf20ccc53a4b77e59ee91bfacc2d8d98444d6d9
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-25 21:46:40 +0300

    [FEATURE] Policy API includes factories
    Factory methods added to Policy API.
    
    == Updates (backward compatible)
    
    * Attestor::Policy (extends Factory module)
    
    == Addons
    
    * Attestor::Policy#and
    * Attestor::Policy#or
    * Attestor::Policy#not
    * Attestor::Policy#xor

--- lib/attestor/policy.rb
+++ lib/attestor/policy.rb
@@ -12,6 +12,7 @@ module Attestor
     def self.included(klass)
       klass.instance_eval do
         include Validations
+        extend Factory
       end
     end
 
@@ -31,6 +32,88 @@ module Attestor
     # @return [Boolean]
     def invalid?
       !valid?
+    end
+
+    # Negates the current policy
+    #
+    # @return [Attestor::Policy::Not]
+    def not
+      self.class.not(self)
+    end
+
+    # Builds the AND composition of current policy with other policies
+    #
+    # @overload and(policy, *others)
+    #   Combines the policy with the others
+    #
+    #   The combination is valid if all policies are valid
+    #
+    #   @param [Attestor::Policy, Array<Attestor::Policy>] others
+    #
+    #   @return [Attestor::Policy::And]
+    #
+    # @overload and(policy)
+    #   Creates a negator object, awaiting fot #not method call
+    #
+    #   @example
+    #     policy.and.not(one, two)
+    #
+    #     # this is equal to combination with negation of other policies:
+    #     policy.and(one.not, two.not)
+    #
+    #   @return [Attestor::Policy::Negator]
+    def and(*others)
+      self.class.and(self, *others)
+    end
+
+    # Builds the OR composition of current policy with other policies
+    #
+    # @overload or(policy, *others)
+    #   Combines the policy with the others
+    #
+    #   The combination is valid unless all the policies are invalid
+    #
+    #   @param [Attestor::Policy, Array<Attestor::Policy>] others
+    #
+    #   @return [Attestor::Policy::And]
+    #
+    # @overload or(policy)
+    #   Creates a negator object, awaiting fot #not method call
+    #
+    #   @example
+    #     policy.or.not(one, two)
+    #
+    #     # this is equal to combination with negation of other policies:
+    #     policy.or(one.not, two.not)
+    #
+    #   @return [Attestor::Policy::Negator]
+    def or(*others)
+      self.class.or(self, *others)
+    end
+
+    # Builds the XOR composition of current policy with other policies
+    #
+    # @overload xor(policy, *others)
+    #   Combines the policy with the others
+    #
+    #   The combination is valid if both valid and invalid policies are present
+    #
+    #   @param [Attestor::Policy, Array<Attestor::Policy>] others
+    #
+    #   @return [Attestor::Policy::And]
+    #
+    # @overload xor(policy)
+    #   Creates a negator object, awaiting fot #not method call
+    #
+    #   @example
+    #     policy.xor.not(one, two)
+    #
+    #     # this is equal to combination with negation of other policies:
+    #     policy.xor(one.not, two.not)
+    #
+    #   @return [Attestor::Policy::Negator]
+    def xor(*others)
+      self.class.xor(self, *others)
     end
 
   end # module Policy
--- spec/tests/policy_spec.rb
+++ spec/tests/policy_spec.rb
@@ -2,13 +2,19 @@
 
 describe Attestor::Policy do
 
+  let(:factory)   { Attestor::Policy::Factory }
   let(:validator) { Attestor::Validations }
   let(:invalid)   { Attestor::InvalidError.new subject, [] }
+  let(:others)    { 2.times { double } }
 
   let(:test_class) { Class.new.send(:include, described_class) }
   before  { Test = test_class }
   after   { Object.send :remove_const, :Test }
   subject { test_class.new }
+
+  it "is a factory" do
+    expect(test_class).to be_kind_of factory
+  end
 
   describe ".new" do
 
@@ -65,5 +71,41 @@ describe Attestor::Policy do
     end
 
   end # describe #invalid?
+
+  describe "#and" do
+
+    it "calls .and class factory method with self" do
+      expect(test_class).to receive(:and).with(subject, *others)
+      subject.and(*others)
+    end
+
+  end # describe #and
+
+  describe "#or" do
+
+    it "calls .or class factory method with self" do
+      expect(test_class).to receive(:or).with(subject, *others)
+      subject.or(*others)
+    end
+
+  end # describe #or
+
+  describe "#xor" do
+
+    it "calls .xor class factory method with self" do
+      expect(test_class).to receive(:xor).with(subject, *others)
+      subject.xor(*others)
+    end
+
+  end # describe #xor
+
+  describe "#not" do
+
+    it "calls .not class factory method with self" do
+      expect(test_class).to receive(:not).with(subject)
+      subject.not
+    end
+
+  end # describe #not
 
 end # describe Attestor::Policy
