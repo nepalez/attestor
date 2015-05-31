1cd17fa7481236b5c0b6e4bdceb646d06ec99b3d
Andrew Kozin <andrew.kozin@gmail.com>
2015-05-31 11:09:21 +0300

    [VERSION] 2.2.1

--- lib/attestor/version.rb
+++ lib/attestor/version.rb
@@ -4,6 +4,6 @@ module Attestor
 
   # The semantic version of the module.
   # @see http://semver.org/ Semantic versioning 2.0
-  VERSION = "2.2.0".freeze
+  VERSION = "2.2.1".freeze
 
 end # module Attestor
